2dfdb8f749074892bdef22077db328b01bf3febd
Aniruddha Tekade <atekade1@binghamton.edu>
2017-10-27 22:50:54 -0400

    Logger and File Processor taken over from assignment 03

--- wordTree/src/wordTree/util/FileProcessor.java
+++ wordTree/src/wordTree/util/FileProcessor.java
@@ -1,9 +1,93 @@
 package wordTree.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
- *
- * @author anirtek
+ * @version 3.0
+ * @author Aniruddha Tekade & Vidhi Kamdar : Submitted on October 19th, 2017.
  */
 public class FileProcessor {
-    
+
+    private String inputFileName = "", outputFileName = "";
+
+    /**
+     * Parameterized constructor of the class that sets the different filename
+     * received from the command line to the local fileName variables.
+     *
+     * @param inputFileIn
+     * @param outputFileIn
+     */
+    public FileProcessor(String inputFileIn, String outputFileIn) {
+        Logger.writeMessage("Constructor called - " + this.toString(), Logger.DebugLevel.CONSTRUCTOR);
+        if (inputFileIn == null) {
+            System.err.println("Input File name is null!");
+            System.exit(0);
+        } else {
+            this.inputFileName = inputFileIn;
+        }
+        if (outputFileIn == null) {
+            System.err.println("Output File name is null!");
+            System.exit(0);
+        } else {
+            this.outputFileName = outputFileIn;
+        }
+    }
+
+    /**
+     * This method performs the reading operation on the input line and
+     * separates the b_number and the courseName facilitating the the easy node
+     * creation in the TreeBuilder class.
+     *
+     * @param sf
+     */
+    public void readLine() {
+        try (BufferedReader lineIn = new BufferedReader(new FileReader(inputFileName))) {
+            try {
+                for (String line; (line = lineIn.readLine()) != null;) {
+                    try {
+                        //sf.processInput(line);
+                    } catch (NumberFormatException ex) {
+                        System.out.println(ex);
+                    }
+                }
+            } catch (IOException ex) {
+                System.out.println(ex);
+            } finally {
+                lineIn.close();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Additional method to write to any file if required.
+     *
+     * @param outputLine
+     */
+    public void writeLine(String outputLine) {
+        try {
+            File outputFile = new File(outputFileName);
+            outputFile.createNewFile();
+            try (PrintWriter pw = new PrintWriter(new FileWriter(outputFile, true))) {
+                pw.println(outputLine);
+            }
+        } catch (IOException ex) {
+            Logger.writeMessage(ex.getMessage(), Logger.DebugLevel.RELEASE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FileProcessor";
+    }
+
 }
--- wordTree/src/wordTree/util/Logger.java
+++ wordTree/src/wordTree/util/Logger.java
@@ -1,9 +1,63 @@
 package wordTree.util;
 
 /**
- *
- * @author anirtek
+ * @version 1.0
+ * @author Aniruddha Tekade Submitted on October 19th, 2017.
  */
 public class Logger {
-    
+
+    /*DEBUG_VALUE=4 [Print to stdout everytime a constructor is called]
+      DEBUG_VALUE=3 [Print to stdout everytime the state is changed]
+      DEBUG_VALUE=2 [Print to stdout everytime a set of operations are performed]
+      DEBUG_VALUE=1 [Print to stdout everytime an exception occurs and handles]
+      DEBUG_VALUE=0 [No output should be printed from the application to stdout. It is ok to write to the output file though" ]
+     */
+    public static enum DebugLevel {
+        RELEASE,
+        FROM_RESULTS,
+        IN_RESULTS,
+        IN_RUN,
+        CONSTRUCTOR
+    };
+
+    private static DebugLevel debugLevel;
+
+    public static void setDebugValue(int levelIn) {
+        switch (levelIn) {
+            case 4:
+                debugLevel = DebugLevel.CONSTRUCTOR;
+                break;
+            case 3:
+                debugLevel = DebugLevel.IN_RUN;
+                break;
+            case 2:
+                debugLevel = DebugLevel.IN_RESULTS;
+                break;
+            case 1:
+                debugLevel = DebugLevel.FROM_RESULTS;
+                break;
+            case 0:
+                debugLevel = DebugLevel.RELEASE;
+                break;
+        }
+    }
+
+    public static void setDebugValue(DebugLevel levelIn) {
+        debugLevel = levelIn;
+    }
+
+    // @return None
+    public static void writeMessage(String message, DebugLevel levelIn) {
+        if (levelIn == debugLevel) {
+            System.out.println(message);
+        } 
+    }
+
+    /**
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "Debug Level is " + debugLevel;
+    }
 }
