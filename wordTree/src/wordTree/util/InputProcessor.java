000a8620efee99e339bee405b388f740e308cce7
Aniruddha Tekade <atekade1@binghamton.edu>
2017-11-07 19:24:46 -0500

    InputProcessor updated with processInput() method

--- wordTree/src/wordTree/util/InputProcessor.java
+++ wordTree/src/wordTree/util/InputProcessor.java
@@ -1,9 +1,17 @@
 package wordTree.util;
 
+import wordTree.myTree.MyTree;
+
 /**
  *
  * @author anirtek
  */
 public class InputProcessor {
+    MyTree myTreeInstance = null;
+    
+    public String[] processInput(String inputLine) {
+        String[] inputPass = inputLine.split("[^a-zA-Z0-9']+");
+        return inputPass;        
+    }
     
 }
