9dfa714771ecb4ed89ddfcc86ba8304d81eed675
Aniruddha Tekade <atekade1@binghamton.edu>
2017-11-07 19:25:49 -0500

    CreateWorker.java methods being added.

--- wordTree/src/wordTree/threadMgmt/CreateWorkers.java
+++ wordTree/src/wordTree/threadMgmt/CreateWorkers.java
@@ -1,9 +1,28 @@
 package wordTree.threadMgmt;
 
+import wordTree.myTree.MyTree;
+import wordTree.store.Results;
+
 /**
  *
  * @author anirtek
  */
 public class CreateWorkers {
+    private MyTree tree = null;
+    private Results results = null;
+
+    public CreateWorkers(MyTree treeIn, Results resultsIn) {
+        this.tree = treeIn;
+        this.results = resultsIn;
+    }
+    
+    public void startPopulateWorkers(int numberOfThreads) {
+        
+        
+    }
+    
+    public void startDeleteWorkers() {
+        
+    }
     
 }
